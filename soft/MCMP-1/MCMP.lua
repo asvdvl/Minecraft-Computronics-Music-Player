697957315912e07a02acea39f40bf5a2ae117beb
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-09-01 00:08:24 +0300

    fix wrong component name. add read more than one byte. complete read and init tape

--- soft/MCMP-1/MCMP.lua
+++ soft/MCMP-1/MCMP.lua
@@ -1,18 +1,18 @@
-local cmp = require("comconent")
+local cmp = require("component")
 local td = cmp.tape_drive
 local formatName = "MCMP"
 local formatVersion = 1
 local tapeInfo = {
-formatName = "",
-formatVersion = 0,
-titlesTableLength = 0,
-titlesTable = {}
-titleItem = {
-"Title",		--name of the track
-0,				--start position
-0,				--end position
-1				--playback speed
-}
+	formatName = "",
+	formatVersion = 0,
+	titlesTableLength = 0,
+	titlesTable = {},
+	titleItem = {
+		"Title",		--name of the track
+		0,				--start position
+		0,				--end position
+		1				--playback speed
+	}
 }
 
 function concatinateBytes(bytes)
@@ -25,24 +25,34 @@ function concatinateBytes(bytes)
 	return concatNum
 end
 
+function readBytes(length)
+	return {string.byte(td.read(length), 1, length)}
+end
+
 function rewind()
 	td.seek(-math.huge)
 end
 
 function initTape()
-	td.seek(-math.huge)
+	rewind()
 	
+	--read info data from tape
 	tapeInfo["formatName"] = td.read(#formatName)
 	tapeInfo["formatVersion"] = td.read()
-	titlesTableLength = 0
+	titlesTableLength = concatinateBytes(readBytes(2))
 	
-	
-	if td.read(#formatName) == formatName then
-		if td.read(#formatVersion)
-		
+	--check on valid tape
+	if tapeInfo["formatName"] == formatName then
+		if tapeInfo["formatVersion"] == formatVersion then
+			io.stdout:write(tapeInfo["formatName"].." ver: "..tostring(tapeInfo["formatVersion"]).."\n")
+			io.stdout:write("Titles table length: "..tostring(titlesTableLength).."\n")
+			
+		else
+			io.stderr:write("Invalid tape: format vertion "..tostring(tapeInfo["formatVersion"]).." not support\n")
 		end
 	else
-		io.stderr:write("Invalid tape: format not support")
+		io.stderr:write("Invalid tape: format "..tapeInfo["formatName"].." not support\n")
 	end
 end
 
+initTape()
