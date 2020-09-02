4daf36c82eb63636078ce2d48741db036e2bede6
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-09-02 23:29:34 +0300

    Add titles table!  add serialization and load titles table.  fix lowercase global functions names.  add some utilites functions.

--- soft/MCMP-1/MCMP.lua
+++ soft/MCMP-1/MCMP.lua
@@ -1,4 +1,5 @@
 local cmp = require("component")
+local ser = require("serialization")
 local td = cmp.tape_drive
 local formatName = "MCMP"
 local formatVersion = 1
@@ -15,7 +16,7 @@ local tapeInfo = {
 	}
 }
 
-function concatinateBytes(bytes)
+function ConcatinateBytes(bytes)
 	local concatNum = 1
 	local secondi = #bytes
 	for _, val in pairs(bytes) do
@@ -25,28 +26,53 @@ function concatinateBytes(bytes)
 	return concatNum
 end
 
-function readBytes(length)
+function ReadBytes(length)
 	return {string.byte(td.read(length), 1, length)}
 end
 
-function rewind()
+function ReadStrig(length)
+	return td.read(length)
+end
+
+function Rewind()
 	td.seek(-math.huge)
 end
 
-function initTape()
-	rewind()
+function ReadTable()
+	return ser.unserialize(ReadStrig(tapeInfo.titlesTableLength))
+end
+
+function SeekToAbsolutlyPosition(position)
+	td.seek(position - td.getPotsition())
+end
+
+function InitTape()
+	Rewind()
 	
 	--read info data from tape
 	tapeInfo["formatName"] = td.read(#formatName)
 	tapeInfo["formatVersion"] = td.read()
-	titlesTableLength = concatinateBytes(readBytes(2))
+	tapeInfo.titlesTableLength = ConcatinateBytes(ReadBytes(2))
 	
 	--check on valid tape
 	if tapeInfo["formatName"] == formatName then
 		if tapeInfo["formatVersion"] == formatVersion then
+			--print base info about table
 			io.stdout:write(tapeInfo["formatName"].." ver: "..tostring(tapeInfo["formatVersion"]).."\n")
-			io.stdout:write("Titles table length: "..tostring(titlesTableLength).."\n")
+			io.stdout:write("Titles table length: "..tostring(tapeInfo.titlesTableLength).."\n")
 			
+			--try to parse table
+			local table, status = ReadTable()
+			if status then
+				io.stderr:write("Error parse titles table: "..status)
+				return
+			end
+			tapeInfo.titlesTable = table
+			--print title table
+			io.stdout:write("track title, start position, end position, playback speed\n")
+			for key, val in pairs(tapeInfo.titlesTable) do
+				io.stdout:write(val[1]..","..val[2]..","..val[3]..","..val[4].."\n")
+			end
 		else
 			io.stderr:write("Invalid tape: format vertion "..tostring(tapeInfo["formatVersion"]).." not support\n")
 		end
@@ -55,4 +81,4 @@ function initTape()
 	end
 end
 
-initTape()
+InitTape()
